d2aea8d6764f424f684b7a5911188c26d78d90c7
samuel-lucas6 <63159663+samuel-lucas6@users.noreply.github.com>
2021-03-21 15:12:58 +0000

    Poly1305.cs: Improve comments.

--- src/Geralt/Poly1305.cs
+++ src/Geralt/Poly1305.cs
@@ -1,5 +1,4 @@
 using Geralt.Exceptions;
-using System.Text;
 
 /*
     Geralt: A cryptographic library for .NET based on libsodium.
@@ -28,65 +27,36 @@ using System.Text;
 namespace Geralt
 {
     /// <summary>Compute a message authentication code using Poly1305.</summary>
-    /// <remarks>See here for more information: https://doc.libsodium.org/advanced/poly1305 </remarks>
     public static class Poly1305
     {
-        private const int _keyBytes = 32;
-        private const int _tagBytes = 16;
-
-        /// <summary>Generates a random 32 byte key.</summary>
-        /// <returns>A byte array with 32 random bytes</returns>
-        public static byte[] GenerateKey()
-        {
-            return SecureRandom.GetBytes(_keyBytes);
-        }
+        public const int KeySize = 32;
+        public const int TagSize = 16;
 
         /// <summary>Computes the message authentication code of a message using Poly1305.</summary>
+        /// <remarks>Never reuse a key. It must be unique for every single message.</remarks>
         /// <param name="message">The message.</param>
-        /// <param name="key">The 32 byte key.</param>
-        /// <returns>A 16 byte authentication tag.</returns>
-        /// <exception cref="KeyOutOfRangeException"></exception>
-        public static byte[] Compute(string message, byte[] key)
-        {
-            return Compute(Encoding.UTF8.GetBytes(message), key);
-        }
-
-        /// <summary>Computes the message authentication code of a message using Poly1305.</summary>
-        /// <param name="message">The message.</param>
-        /// <param name="key">The 32 byte key.</param>
+        /// <param name="key">The 32 byte one-time key.</param>
         /// <returns>A 16 byte authentication tag.</returns>
         /// <exception cref="KeyOutOfRangeException"></exception>
         public static byte[] Compute(byte[] message, byte[] key)
         {
-            ParameterValidation.Key(key, _keyBytes);
-            byte[] tag = new byte[_tagBytes];
+            ParameterValidation.Key(key, KeySize);
+            byte[] tag = new byte[TagSize];
             _ = LibsodiumLibrary.crypto_onetimeauth(tag, message, message.Length, key);
             return tag;
         }
 
-        /// <summary>Verifies a Poly1305 message authentication code.</summary>
+        /// <summary>Verifies a Poly1305 authentication tag.</summary>
         /// <param name="message">The message.</param>
         /// <param name="tag">The 16 byte authentication tag.</param>
-        /// <param name="key">The 32 byte key.</param>
-        /// <returns>True if verified.</returns>
-        /// <exception cref="KeyOutOfRangeException"></exception>
-        /// <exception cref="SignatureOutOfRangeException"></exception>
-        public static bool Verify(string message, byte[] tag, byte[] key)
-        {
-            return Verify(Encoding.UTF8.GetBytes(message), tag, key);
-        }
-
-        /// <summary>Verifies a Poly1305 message authentication code.</summary>
-        /// <param name="message">The message.</param>
-        /// <param name="tag">The 16 byte authentication tag.</param>
-        /// <param name="key">The 32 byte key.</param>
-        /// <returns>True if verified.</returns>
+        /// <param name="key">The 32 byte one-time key.</param>
+        /// <returns><see langword="true"/> if valid; otherwise, <see langword="false"/>.</returns>
         /// <exception cref="KeyOutOfRangeException"></exception>
         /// <exception cref="SignatureOutOfRangeException"></exception>
         public static bool Verify(byte[] message, byte[] tag, byte[] key)
         {
-            ParameterValidation.Key(key, _keyBytes);
-            ParameterValidation.Tag(tag, _tagBytes);
+            ParameterValidation.Key(key, KeySize);
+            ParameterValidation.Tag(tag, TagSize);
             int result = LibsodiumLibrary.crypto_onetimeauth_verify(tag, message, message.Length, key);
             return result == 0;
         }
