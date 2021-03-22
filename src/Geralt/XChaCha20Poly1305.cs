b0a92bc8ae474b3fe2bf69067ed97bc89e9bb244
samuel-lucas6 <63159663+samuel-lucas6@users.noreply.github.com>
2021-03-22 14:17:50 +0000

    XChaCha20Poly1305.cs: Improve comments.

--- src/Geralt/XChaCha20Poly1305.cs
+++ src/Geralt/XChaCha20Poly1305.cs
@@ -30,57 +30,35 @@ using System.Security.Cryptography;
 namespace Geralt
 {
     /// <summary>Authenticated encryption with additional data using XChaCha20-Poly1305.</summary>
-    /// <remarks>See here for more information: https://doc.libsodium.org/secret-key_cryptography/aead/chacha20-poly1305/xchacha20-poly1305_construction </remarks>
     public static class XChaCha20Poly1305
     {
-        private const int _keyBytes = 32;
-        private const int _nonceBytes = 24;
-        private const int _tagBytes = 16;
-
-        /// <summary>Generates a random 32 byte key.</summary>
-        /// <returns>A byte array with 32 random bytes.</returns>
-        public static byte[] GenerateKey()
-        {
-            return SecureRandom.GetBytes(_keyBytes);
-        }
-
-        /// <summary>Generates a random 24 byte nonce.</summary>
-        /// <returns>A byte array with 24 random bytes.</returns>
-        public static byte[] GenerateNonce()
-        {
-            return SecureRandom.GetBytes(_nonceBytes);
-        }
-
-        /// <summary>Increments a nonce in constant time.</summary>
-        /// <param name="nonce">The nonce to increment.</param>
-        /// <returns>The incremented byte array.</returns>
-        public static byte[] IncrementNonce(byte[] nonce)
-        {
-            return ConstantTime.Increment(nonce);
-        }
+        public const int KeySize = 32;
+        public const int NonceSize = 24;
+        public const int TagSize = 16;
 
         /// <summary>Encrypts a message using XChaCha20-Poly1305.</summary>
+        /// <remarks>The nonce should never be reused with the same key. It must be incremented in constant time.</remarks>
         /// <param name="message">The message to be encrypted.</param>
         /// <param name="nonce">The 24 byte nonce.</param>
         /// <param name="key">The 32 byte key.</param>
-        /// <param name="additionalData">The additional data - can be null.</param>
-        /// <returns>The encrypted message with an authentication tag.</returns>
-        /// <remarks>The nonce should never be reused with the same key.</remarks>
-        /// <remarks>The recommended way to generate a nonce is to use GenerateNonce() for the first message and increment it for each subsequent message using the same key.</remarks>
-        /// <exception cref="KeyOutOfRangeException"></exception>
+        /// <param name="additionalData">Optional, non-secret additional data to authenticate.</param>
+        /// <returns>The encrypted message and authentication tag.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="NonceOutOfRangeException"></exception>
+        /// <exception cref="KeyOutOfRangeException"></exception>
         /// <exception cref="CryptographicException"></exception>
         public static byte[] Encrypt(byte[] message, byte[] nonce, byte[] key, byte[] additionalData = null)
         {
+            ParameterValidation.Message(message);
+            ParameterValidation.Nonce(nonce, NonceSize);
+            ParameterValidation.Key(key, KeySize);
             additionalData = ParameterValidation.AdditionalData(additionalData);
-            ParameterValidation.Nonce(nonce, _nonceBytes);
-            ParameterValidation.Key(key, _keyBytes);
-            byte[] ciphertext = new byte[message.Length + _tagBytes];
+            byte[] ciphertext = new byte[message.Length + TagSize];
             IntPtr ciphertextPointer = Marshal.AllocHGlobal(ciphertext.Length);
             int result = LibsodiumLibrary.crypto_aead_xchacha20poly1305_ietf_encrypt(ciphertextPointer, out long ciphertextLength, message, message.Length, additionalData, additionalData.Length, nsec: null, nonce, key);
             Marshal.Copy(ciphertextPointer, ciphertext, startIndex: 0, (int)ciphertextLength);
             Marshal.FreeHGlobal(ciphertextPointer);
-            ResultValidation.EncryptResult(result);
+            ResultValidation.EncryptionResult(result);
             return ciphertext.Length == ciphertextLength ? ciphertext : NullPadding.RemoveTrailingNulls(ciphertext, ciphertextLength);
         }
 
@@ -88,22 +66,24 @@ namespace Geralt
         /// <param name="ciphertext">The ciphertext to be decrypted.</param>
         /// <param name="nonce">The 24 byte nonce.</param>
         /// <param name="key">The 32 byte key.</param>
-        /// <param name="additionalData">The additional data - can be null.</param>
-        /// <returns>The decrypted ciphertext.</returns>
-        /// <exception cref="KeyOutOfRangeException"></exception>
+        /// <param name="additionalData">Optional, non-secret additional data to authenticate.</param>
+        /// <returns>The decrypted message.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="NonceOutOfRangeException"></exception>
+        /// <exception cref="KeyOutOfRangeException"></exception>
         /// <exception cref="CryptographicException"></exception>
         public static byte[] Decrypt(byte[] ciphertext, byte[] nonce, byte[] key, byte[] additionalData = null)
         {
+            ParameterValidation.Ciphertext(ciphertext);
+            ParameterValidation.Nonce(nonce, NonceSize);
+            ParameterValidation.Key(key, KeySize);
             additionalData = ParameterValidation.AdditionalData(additionalData);
-            ParameterValidation.Nonce(nonce, _nonceBytes);
-            ParameterValidation.Key(key, _keyBytes);
-            byte[] plaintext = new byte[ciphertext.Length - _tagBytes];
+            byte[] plaintext = new byte[ciphertext.Length - TagSize];
             IntPtr plaintextPointer = Marshal.AllocHGlobal(plaintext.Length);
             int result = LibsodiumLibrary.crypto_aead_xchacha20poly1305_ietf_decrypt(plaintextPointer, out long plaintextLength, nsec: null, ciphertext, ciphertext.Length, additionalData, additionalData.Length, nonce, key);
             Marshal.Copy(plaintextPointer, plaintext, startIndex: 0, (int)plaintextLength);
             Marshal.FreeHGlobal(plaintextPointer);
-            ResultValidation.DecryptResult(result);
+            ResultValidation.DecryptionResult(result);
             return plaintext.Length == plaintextLength ? plaintext : NullPadding.RemoveTrailingNulls(plaintext, plaintextLength);
         }
     }
