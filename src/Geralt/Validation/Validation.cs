f19b9b5ede0700d96a14c91d4dd2fa3dbbf454b3
samuel-lucas6 <63159663+samuel-lucas6@users.noreply.github.com>
2022-11-19 22:15:46 +0000

    Validation.cs: Make public.
    Useful for custom constructions.

--- src/Geralt/Validation/Validation.cs
+++ src/Geralt/Validation/Validation.cs
@@ -1,38 +1,44 @@
 ﻿namespace Geralt;
 
-internal static class Validation
+public static class Validation
 {
-    internal static void EqualToSize(string paramName, int size, int validSize)
+    public static void EqualToSize(string paramName, int size, int validSize)
     {
         if (size != validSize)
             throw new ArgumentOutOfRangeException(paramName, size, $"{paramName} must be {validSize} bytes long.");
     }
 
-    internal static void SizeBetween(string paramName, int size, int minSize, int maxSize)
+    public static void SizeBetween(string paramName, int size, int minSize, int maxSize)
     {
         if (size < minSize || size > maxSize)
             throw new ArgumentOutOfRangeException(paramName, size, $"{paramName} must be between {minSize} and {maxSize} bytes long.");
     }
 
-    internal static void GreaterThanZero(string paramName, int size)
+    public static void GreaterThanZero(string paramName, int size)
     {
         if (size <= 0)
             throw new ArgumentOutOfRangeException(paramName, size, $"{paramName} must be greater than 0.");
     }
 
-    internal static void NotLessThanMin(string paramName, int size, int minSize)
+    public static void NotLessThanMin(string paramName, int size, int minSize)
     {
         if (size < minSize)
             throw new ArgumentOutOfRangeException(paramName, size, $"{paramName} must be equal to or greater than {minSize}.");
     }
 
-    internal static void NotEmpty(string paramName, int size)
+    public static void NotEmpty(string paramName, int size)
     {
         if (size == 0)
             throw new ArgumentOutOfRangeException(paramName, size, $"{paramName} cannot have a length of 0.");
     }
+    
+    public static void NotNull<T>(string paramName, T value) where T: class
+    {
+        if (value == null)
+            throw new ArgumentNullException(paramName, $"{paramName} cannot be null.");
+    }
 
-    internal static void NotNullOrEmpty(string paramName, string value)
+    public static void NotNullOrEmpty(string paramName, string value)
     {
         if (value == null)
             throw new ArgumentNullException(paramName, $"{paramName} cannot be null.");
