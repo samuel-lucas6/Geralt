0a06bf93d42bdc9afb40d48d261c0cd6db4549e8
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2023-01-20 22:14:04 +0000

    SecureRandomTests.cs: Use DataRow.

--- src/Geralt.Tests/SecureRandomTests.cs
+++ src/Geralt.Tests/SecureRandomTests.cs
@@ -8,128 +8,98 @@ namespace Geralt.Tests;
 public class SecureRandomTests
 {
     [TestMethod]
-    public void Fill_NotAllZeros()
+    public void Fill_Valid()
     {
         Span<byte> buffer = stackalloc byte[ChaCha20Poly1305.KeySize];
+        
         SecureRandom.Fill(buffer);
+        
         Assert.IsFalse(buffer.SequenceEqual(new byte[buffer.Length]));
     }
     
     [TestMethod]
-    public void Fill_InvalidBuffer()
+    public void Fill_Invalid()
     {
         var buffer = Array.Empty<byte>();
+        
         Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.Fill(buffer));
     }
     
     [TestMethod]
-    public void FillDeterministic_NotAllZeros()
+    public void FillDeterministic_Valid()
     {
         Span<byte> buffer = stackalloc byte[ChaCha20Poly1305.KeySize];
         Span<byte> seed = stackalloc byte[SecureRandom.SeedSize];
+        
         SecureRandom.FillDeterministic(buffer, seed);
+        
         Assert.IsFalse(buffer.SequenceEqual(new byte[buffer.Length]));
     }
     
     [TestMethod]
-    public void FillDeterministic_InvalidBuffer()
+    [DataRow(0, SecureRandom.SeedSize)]
+    [DataRow(ChaCha20Poly1305.KeySize, SecureRandom.SeedSize + 1)]
+    [DataRow(ChaCha20Poly1305.KeySize, SecureRandom.SeedSize - 1)]
+    public void FillDeterministic_Invalid(int bufferSize, int seedSize)
     {
-        var buffer = Array.Empty<byte>();
-        var seed = new byte[SecureRandom.SeedSize];
+        var buffer = new byte[bufferSize];
+        var seed = new byte[seedSize];
+        
         Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.FillDeterministic(buffer, seed));
     }
     
     [TestMethod]
-    public void FillDeterministic_InvalidSeed()
-    {
-        var buffer = new byte[ChaCha20Poly1305.KeySize];
-        var seed = new byte[SecureRandom.SeedSize - 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.FillDeterministic(buffer, seed));
-        seed = new byte[SecureRandom.SeedSize + 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.FillDeterministic(buffer, seed));
-    }
-    
-    [TestMethod]
-    public void GetInt32_ValidUpperBound()
+    public void GetInt32_Valid()
     {
         int number = SecureRandom.GetInt32(SecureRandom.MinUpperBound);
-        Assert.IsTrue(number < SecureRandom.MinUpperBound);
-        Assert.IsTrue(number >= 0);
+        
+        Assert.IsTrue(number is < SecureRandom.MinUpperBound and >= 0);
     }
     
     [TestMethod]
-    public void GetInt32_InvalidUpperBound()
+    public void GetInt32_Invalid()
     {
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.GetInt32(SecureRandom.MinUpperBound - 1));
+        int upperBound = SecureRandom.MinUpperBound - 1;
+        
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.GetInt32(upperBound));
     }
     
     [TestMethod]
-    public void GetPassphrase_ValidDefaultChars()
+    public void GetPassphrase_Valid()
     {
-        const char separatorChar = '-';
-        char[] passphrase = SecureRandom.GetPassphrase(SecureRandom.MinWordCount);
-        Assert.IsTrue(passphrase.Contains(separatorChar));
-        Assert.IsTrue(char.IsLower(passphrase[0]));
-        Assert.IsFalse(passphrase[^1] == separatorChar);
-        Assert.IsFalse(passphrase.Any(char.IsDigit));
-    }
-    
-    [TestMethod]
-    public void GetPassphrase_ValidCustomChars()
-    {
-        const char separatorChar = '=';
+        const char separatorChar = ' ';
         char[] passphrase = SecureRandom.GetPassphrase(SecureRandom.MinWordCount, separatorChar, capitalise: false, includeNumber: true);
+        
         Assert.IsFalse(passphrase.Contains('-'));
         Assert.IsTrue(passphrase.Contains(separatorChar));
-        Assert.IsTrue(char.IsLower(passphrase[0]));
         Assert.IsFalse(passphrase[^1] == separatorChar);
+        Assert.IsTrue(char.IsLower(passphrase[0]));
         Assert.IsTrue(passphrase.Any(char.IsDigit));
     }
     
     [TestMethod]
-    public void GetPassphrase_InvalidWordCount()
+    [DataRow(SecureRandom.MaxWordCount + 1)]
+    [DataRow(SecureRandom.MinWordCount - 1)]
+    public void GetPassphrase_Invalid(int wordCount)
     {
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.GetPassphrase(SecureRandom.MinWordCount - 1));
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.GetPassphrase(SecureRandom.MaxWordCount + 1));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.GetPassphrase(wordCount));
     }
     
     [TestMethod]
-    public void GetString_ValidAlphabeticChars()
+    public void GetString_Valid()
     {
-        string randomFileName = SecureRandom.GetString(SecureRandom.MaxStringLength, SecureRandom.AlphabeticChars);
-        Assert.IsTrue(randomFileName.All(char.IsLetter));
+        int length = SecureRandom.MaxStringLength;
+        string random = SecureRandom.GetString(length, SecureRandom.AlphabeticChars);
+        
+        Assert.IsTrue(random.Length == length);
+        Assert.IsTrue(random.All(char.IsLetter));
     }
     
     [TestMethod]
-    public void GetString_ValidNumericChars()
+    [DataRow(SecureRandom.MaxStringLength + 1)]
+    [DataRow(SecureRandom.MinStringLength - 1)]
+    public void GetString_Invalid(int length)
     {
-        string randomFileName = SecureRandom.GetString(SecureRandom.MaxStringLength, SecureRandom.NumericChars);
-        Assert.IsTrue(randomFileName.All(char.IsDigit));
-    }
-    
-    [TestMethod]
-    public void GetString_ValidAlphanumericChars()
-    {
-        string randomFileName = SecureRandom.GetString(SecureRandom.MaxStringLength, SecureRandom.AlphanumericChars);
-        Assert.IsFalse(randomFileName.Any(char.IsSymbol));
-        Assert.IsFalse(randomFileName.Any(char.IsPunctuation));
-        Assert.IsTrue(randomFileName.Any(char.IsDigit));
-        Assert.IsTrue(randomFileName.Any(char.IsLetter));
-    }
-    
-    [TestMethod]
-    public void GetString_ValidAlphanumericSymbolChars()
-    {
-        string randomFileName = SecureRandom.GetString(SecureRandom.MaxStringLength, SecureRandom.AlphanumericSymbolChars);
-        Assert.IsTrue(randomFileName.Any(char.IsSymbol) || randomFileName.Any(char.IsPunctuation));
-        Assert.IsTrue(randomFileName.Any(char.IsDigit));
-        Assert.IsTrue(randomFileName.Any(char.IsLetter));
-    }
-    
-    [TestMethod]
-    public void GetString_InvalidLength()
-    {
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.GetString(SecureRandom.MinStringLength - 1));
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.GetString(SecureRandom.MaxStringLength + 1));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => SecureRandom.GetString(length));
     }
 }
