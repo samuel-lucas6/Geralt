cf24d2dec40f976b8546f88585629ad6d3b3b812
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2023-01-20 21:12:11 +0000

    ChaCha20Poly1305Tests.cs: Use DynamicData.

--- src/Geralt.Tests/ChaCha20Poly1305Tests.cs
+++ src/Geralt.Tests/ChaCha20Poly1305Tests.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,179 +8,110 @@ namespace Geralt.Tests;
 [TestClass]
 public class ChaCha20Poly1305Tests
 {
-    // RFC 8439 Section 2.8.2: https://datatracker.ietf.org/doc/html/rfc8439#section-2.8.2
-    private static readonly byte[] Plaintext = Convert.FromHexString("4c616469657320616e642047656e746c656d656e206f662074686520636c617373206f66202739393a204966204920636f756c64206f6666657220796f75206f6e6c79206f6e652074697020666f7220746865206675747572652c2073756e73637265656e20776f756c642062652069742e");
-    private static readonly byte[] Ciphertext = Convert.FromHexString("d31a8d34648e60db7b86afbc53ef7ec2a4aded51296e08fea9e2b5a736ee62d63dbea45e8ca9671282fafb69da92728b1a71de0a9e060b2905d6a5b67ecd3b3692ddbd7f2d778b8c9803aee328091b58fab324e4fad675945585808b4831d7bc3ff4def08e4b7a9de576d26586cec64b61161ae10b594f09e26a7e902ecbd0600691");
-    private static readonly byte[] Nonce = Convert.FromHexString("070000004041424344454647");
-    private static readonly byte[] Key = Convert.FromHexString("808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f");
-    private static readonly byte[] AssociatedData = Convert.FromHexString("50515253c0c1c2c3c4c5c6c7");
-    
-    [TestMethod]
-    public void Encrypt_ValidInputs()
+    // https://datatracker.ietf.org/doc/html/rfc8439#section-2.8.2
+    // https://datatracker.ietf.org/doc/html/rfc8439#appendix-A.5
+    public static IEnumerable<object[]> Rfc8439TestVectors()
     {
-        Span<byte> ciphertext = stackalloc byte[Plaintext.Length + ChaCha20Poly1305.TagSize];
-        ChaCha20Poly1305.Encrypt(ciphertext, Plaintext, Nonce, Key, AssociatedData);
-        Assert.IsTrue(ciphertext.SequenceEqual(Ciphertext));
+        yield return new object[]
+        {
+            "d31a8d34648e60db7b86afbc53ef7ec2a4aded51296e08fea9e2b5a736ee62d63dbea45e8ca9671282fafb69da92728b1a71de0a9e060b2905d6a5b67ecd3b3692ddbd7f2d778b8c9803aee328091b58fab324e4fad675945585808b4831d7bc3ff4def08e4b7a9de576d26586cec64b61161ae10b594f09e26a7e902ecbd0600691",
+            "4c616469657320616e642047656e746c656d656e206f662074686520636c617373206f66202739393a204966204920636f756c64206f6666657220796f75206f6e6c79206f6e652074697020666f7220746865206675747572652c2073756e73637265656e20776f756c642062652069742e",
+            "070000004041424344454647",
+            "808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f",
+            "50515253c0c1c2c3c4c5c6c7"
+        };
+        yield return new object[]
+        {
+            "64a0861575861af460f062c79be643bd5e805cfd345cf389f108670ac76c8cb24c6cfc18755d43eea09ee94e382d26b0bdb7b73c321b0100d4f03b7f355894cf332f830e710b97ce98c8a84abd0b948114ad176e008d33bd60f982b1ff37c8559797a06ef4f0ef61c186324e2b3506383606907b6a7c02b0f9f6157b53c867e4b9166c767b804d46a59b5216cde7a4e99040c5a40433225ee282a1b0a06c523eaf4534d7f83fa1155b0047718cbc546a0d072b04b3564eea1b422273f548271a0bb2316053fa76991955ebd63159434ecebb4e466dae5a1073a6727627097a1049e617d91d361094fa68f0ff77987130305beaba2eda04df997b714d6c6f2c29a6ad5cb4022b02709beead9d67890cbb22392336fea1851f38",
+            "496e7465726e65742d4472616674732061726520647261667420646f63756d656e74732076616c696420666f722061206d6178696d756d206f6620736978206d6f6e74687320616e64206d617920626520757064617465642c207265706c616365642c206f72206f62736f6c65746564206279206f7468657220646f63756d656e747320617420616e792074696d652e20497420697320696e617070726f70726961746520746f2075736520496e7465726e65742d447261667473206173207265666572656e6365206d6174657269616c206f7220746f2063697465207468656d206f74686572207468616e206173202fe2809c776f726b20696e2070726f67726573732e2fe2809d",
+            "000000000102030405060708",
+            "1c9240a5eb55d38af333888604f6b5f0473917c1402b80099dca5cbc207075c0",
+            "f33388860000000000004e91"
+        };
     }
     
-    [TestMethod]
-    public void Encrypt_EmptyPlaintext()
+    public static IEnumerable<object[]> InvalidParameterSizes()
     {
-        Span<byte> plaintext = Span<byte>.Empty;
-        Span<byte> ciphertext = stackalloc byte[plaintext.Length + ChaCha20Poly1305.TagSize];
-        ChaCha20Poly1305.Encrypt(ciphertext, plaintext, Nonce, Key);
-        Assert.IsFalse(ciphertext.SequenceEqual(new byte[ciphertext.Length]));
-        Span<byte> decrypted = stackalloc byte[plaintext.Length];
-        ChaCha20Poly1305.Decrypt(decrypted, ciphertext, Nonce, Key);
-        Assert.IsTrue(plaintext.SequenceEqual(decrypted));
-    }
-    
-    [TestMethod]
-    public void Encrypt_DifferentNonce()
-    {
-        Span<byte> ciphertext = stackalloc byte[Plaintext.Length + ChaCha20Poly1305.TagSize];
-        Span<byte> nonce = Nonce.ToArray();
-        nonce[0]++;
-        ChaCha20Poly1305.Encrypt(ciphertext, Plaintext, nonce, Key);
-        Assert.IsFalse(ciphertext.SequenceEqual(Ciphertext));
+        yield return new object[] { ChaCha20Poly1305.TagSize, 1, ChaCha20Poly1305.NonceSize, ChaCha20Poly1305.KeySize, ChaCha20Poly1305.TagSize };
+        yield return new object[] { ChaCha20Poly1305.TagSize, 0, ChaCha20Poly1305.NonceSize + 1, ChaCha20Poly1305.KeySize, ChaCha20Poly1305.TagSize };
+        yield return new object[] { ChaCha20Poly1305.TagSize, 0, ChaCha20Poly1305.NonceSize - 1, ChaCha20Poly1305.KeySize, ChaCha20Poly1305.TagSize };
+        yield return new object[] { ChaCha20Poly1305.TagSize, 0, ChaCha20Poly1305.NonceSize, ChaCha20Poly1305.KeySize + 1, ChaCha20Poly1305.TagSize };
+        yield return new object[] { ChaCha20Poly1305.TagSize, 0, ChaCha20Poly1305.NonceSize, ChaCha20Poly1305.KeySize - 1, ChaCha20Poly1305.TagSize };
     }
     
     [TestMethod]
-    public void Encrypt_DifferentKey()
+    [DynamicData(nameof(Rfc8439TestVectors), DynamicDataSourceType.Method)]
+    public void Encrypt_Valid(string ciphertext, string plaintext, string nonce, string key, string associatedData)
     {
-        Span<byte> ciphertext = stackalloc byte[Plaintext.Length + ChaCha20Poly1305.TagSize];
-        Span<byte> key = Key.ToArray();
-        key[0]++;
-        ChaCha20Poly1305.Encrypt(ciphertext, Plaintext, Nonce, key);
-        Assert.IsFalse(ciphertext.SequenceEqual(Ciphertext));
+        Span<byte> p = Convert.FromHexString(plaintext);
+        Span<byte> n = Convert.FromHexString(nonce);
+        Span<byte> k = Convert.FromHexString(key);
+        Span<byte> a = Convert.FromHexString(associatedData);
+        Span<byte> c = stackalloc byte[p.Length + ChaCha20Poly1305.TagSize];
+        
+        ChaCha20Poly1305.Encrypt(c, p, n, k, a);
+        
+        Assert.AreEqual(ciphertext, Convert.ToHexString(c).ToLower());
     }
     
     [TestMethod]
-    public void Encrypt_DifferentAssociatedData()
+    [DynamicData(nameof(InvalidParameterSizes), DynamicDataSourceType.Method)]
+    public void Encrypt_Invalid(int ciphertextSize, int plaintextSize, int nonceSize, int keySize, int associatedDataSize)
     {
-        Span<byte> ciphertext = stackalloc byte[Plaintext.Length + ChaCha20Poly1305.TagSize];
-        Span<byte> associatedData = AssociatedData.ToArray();
-        associatedData[0]++;
-        ChaCha20Poly1305.Encrypt(ciphertext, Plaintext, Nonce, Key, associatedData);
-        Assert.IsFalse(ciphertext.SequenceEqual(Ciphertext));
+        var c = new byte[ciphertextSize];
+        var p = new byte[plaintextSize];
+        var n = new byte[nonceSize];
+        var k = new byte[keySize];
+        var a = new byte[associatedDataSize];
+        
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Encrypt(c, p, n, k, a));
     }
     
     [TestMethod]
-    public void Encrypt_InvalidCiphertext()
+    [DynamicData(nameof(Rfc8439TestVectors), DynamicDataSourceType.Method)]
+    public void Decrypt_Valid(string ciphertext, string plaintext, string nonce, string key, string associatedData)
     {
-        var ciphertext = new byte[Plaintext.Length + ChaCha20Poly1305.TagSize + 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Encrypt(ciphertext, Plaintext, Nonce, Key));
-        ciphertext = new byte[Plaintext.Length + ChaCha20Poly1305.TagSize - 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Encrypt(ciphertext, Plaintext, Nonce, Key));
-    }
-
-    [TestMethod]
-    public void Encrypt_InvalidNonce()
-    {
-        var ciphertext = new byte[Plaintext.Length + ChaCha20Poly1305.TagSize];
-        var nonce = new byte[ChaCha20Poly1305.NonceSize - 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Encrypt(ciphertext, Plaintext, nonce, Key));
-        nonce = new byte[ChaCha20Poly1305.NonceSize + 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Encrypt(ciphertext, Plaintext, nonce, Key));
+        Span<byte> c = Convert.FromHexString(ciphertext);
+        Span<byte> n = Convert.FromHexString(nonce);
+        Span<byte> k = Convert.FromHexString(key);
+        Span<byte> a = Convert.FromHexString(associatedData);
+        Span<byte> p = stackalloc byte[c.Length - ChaCha20Poly1305.TagSize];
+        
+        ChaCha20Poly1305.Decrypt(p, c, n, k, a);
+        
+        Assert.AreEqual(plaintext, Convert.ToHexString(p).ToLower());
     }
     
     [TestMethod]
-    public void Encrypt_InvalidKey()
+    [DynamicData(nameof(Rfc8439TestVectors), DynamicDataSourceType.Method)]
+    public void Decrypt_Tampered(string ciphertext, string plaintext, string nonce, string key, string associatedData)
     {
-        var ciphertext = new byte[Plaintext.Length + ChaCha20Poly1305.TagSize];
-        var key = new byte[ChaCha20Poly1305.KeySize - 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Encrypt(ciphertext, Plaintext, Nonce, key));
-        key = new byte[ChaCha20Poly1305.KeySize + 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Encrypt(ciphertext, Plaintext, Nonce, key));
+        var parameters = new List<byte[]>
+        {
+            Convert.FromHexString(ciphertext),
+            Convert.FromHexString(nonce),
+            Convert.FromHexString(key),
+            Convert.FromHexString(associatedData)
+        };
+        var p = new byte[parameters[0].Length - ChaCha20Poly1305.TagSize];
+        
+        foreach (var param in parameters) {
+            param[0]++;
+            Assert.ThrowsException<CryptographicException>(() => ChaCha20Poly1305.Decrypt(p, parameters[0], parameters[1], parameters[2], parameters[3]));
+            param[0]--;
+        }
     }
     
     [TestMethod]
-    public void Decrypt_ValidInputs()
+    [DynamicData(nameof(InvalidParameterSizes), DynamicDataSourceType.Method)]
+    public void Decrypt_Invalid(int ciphertextSize, int plaintextSize, int nonceSize, int keySize, int associatedDataSize)
     {
-        Span<byte> plaintext = stackalloc byte[Ciphertext.Length - ChaCha20Poly1305.TagSize];
-        ChaCha20Poly1305.Decrypt(plaintext, Ciphertext, Nonce, Key, AssociatedData);
-        Assert.IsTrue(plaintext.SequenceEqual(Plaintext));
-    }
-
-    [TestMethod]
-    public void Decrypt_WrongCiphertext()
-    {
-        var plaintext = new byte[Ciphertext.Length - ChaCha20Poly1305.TagSize];
-        var ciphertext = Ciphertext.ToArray();
-        ciphertext[0]++;
-        Assert.ThrowsException<CryptographicException>(() => ChaCha20Poly1305.Decrypt(plaintext, ciphertext, Nonce, Key, AssociatedData));
-    }
-    
-    [TestMethod]
-    public void Decrypt_WrongTag()
-    {
-        var plaintext = new byte[Ciphertext.Length - ChaCha20Poly1305.TagSize];
-        var ciphertext = Ciphertext.ToArray();
-        ciphertext[^1]++;
-        Assert.ThrowsException<CryptographicException>(() => ChaCha20Poly1305.Decrypt(plaintext, ciphertext, Nonce, Key, AssociatedData));
-    }
-    
-    [TestMethod]
-    public void Decrypt_WrongNonce()
-    {
-        var plaintext = new byte[Ciphertext.Length - ChaCha20Poly1305.TagSize];
-        var nonce = Nonce.ToArray();
-        nonce[0]++;
-        Assert.ThrowsException<CryptographicException>(() => ChaCha20Poly1305.Decrypt(plaintext, Ciphertext, nonce, Key, AssociatedData));
-    }
-    
-    [TestMethod]
-    public void Decrypt_WrongKey()
-    {
-        var plaintext = new byte[Ciphertext.Length - ChaCha20Poly1305.TagSize];
-        var key = Key.ToArray();
-        key[0]++;
-        Assert.ThrowsException<CryptographicException>(() => ChaCha20Poly1305.Decrypt(plaintext, Ciphertext, Nonce, key, AssociatedData));
-    }
-    
-    [TestMethod]
-    public void Decrypt_WrongAssociatedData()
-    {
-        var plaintext = new byte[Ciphertext.Length - ChaCha20Poly1305.TagSize];
-        var associatedData = AssociatedData.ToArray();
-        associatedData[0]++;
-        Assert.ThrowsException<CryptographicException>(() => ChaCha20Poly1305.Decrypt(plaintext, Ciphertext, Nonce, Key, associatedData));
-    }
-    
-    [TestMethod]
-    public void Decrypt_InvalidPlaintext()
-    {
-        var plaintext = new byte[Ciphertext.Length - ChaCha20Poly1305.TagSize + 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Decrypt(plaintext, Ciphertext, Nonce, Key));
-        plaintext = new byte[Ciphertext.Length - ChaCha20Poly1305.TagSize - 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Decrypt(plaintext, Ciphertext, Nonce, Key));
-    }
-    
-    [TestMethod]
-    public void Decrypt_InvalidCiphertext()
-    {
-        var plaintext = new byte[Ciphertext.Length - ChaCha20Poly1305.TagSize];
-        var ciphertext = new byte[ChaCha20Poly1305.TagSize - 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Decrypt(plaintext, ciphertext, Nonce, Key));
-    }
-
-    [TestMethod]
-    public void Decrypt_InvalidNonce()
-    {
-        var plaintext = new byte[Ciphertext.Length - ChaCha20Poly1305.TagSize];
-        var nonce = new byte[ChaCha20Poly1305.NonceSize - 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Decrypt(plaintext, Ciphertext, nonce, Key));
-        nonce = new byte[ChaCha20Poly1305.NonceSize + 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Decrypt(plaintext, Ciphertext, nonce, Key));
-    }
-    
-    [TestMethod]
-    public void Decrypt_InvalidKey()
-    {
-        var plaintext = new byte[Ciphertext.Length - ChaCha20Poly1305.TagSize];
-        var key = new byte[ChaCha20Poly1305.KeySize - 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Decrypt(plaintext, Ciphertext, Nonce, key));
-        key = new byte[ChaCha20Poly1305.KeySize + 1];
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Decrypt(plaintext, Ciphertext, Nonce, key));
+        var c = new byte[ciphertextSize];
+        var p = new byte[plaintextSize];
+        var n = new byte[nonceSize];
+        var k = new byte[keySize];
+        var a = new byte[associatedDataSize];
+        
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => ChaCha20Poly1305.Decrypt(p, c, n, k, a));
     }
 }
