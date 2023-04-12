161e33396b838ab698074241ec6572ef0045e050
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2023-04-12 20:56:01 +0100

    Ed25519.cs: Rename X25519 conversion parameters.

--- src/Geralt/Crypto/Ed25519.cs
+++ src/Geralt/Crypto/Ed25519.cs
@@ -47,24 +47,24 @@ public static class Ed25519
         }
     }
 
-    public static unsafe void GetX25519PublicKey(Span<byte> X25519PublicKey, ReadOnlySpan<byte> Ed25519PublicKey)
+    public static unsafe void GetX25519PublicKey(Span<byte> x25519PublicKey, ReadOnlySpan<byte> ed25519PublicKey)
     {
-        Validation.EqualToSize(nameof(X25519PublicKey), X25519PublicKey.Length, X25519.PublicKeySize);
-        Validation.EqualToSize(nameof(Ed25519PublicKey), Ed25519PublicKey.Length, PublicKeySize);
+        Validation.EqualToSize(nameof(x25519PublicKey), x25519PublicKey.Length, X25519.PublicKeySize);
+        Validation.EqualToSize(nameof(ed25519PublicKey), ed25519PublicKey.Length, PublicKeySize);
         Sodium.Initialize();
-        fixed (byte* x = X25519PublicKey, e = Ed25519PublicKey)
+        fixed (byte* x = x25519PublicKey, e = ed25519PublicKey)
         {
             int ret = crypto_sign_ed25519_pk_to_curve25519(x, e);
             if (ret != 0) { throw new CryptographicException("Unable to compute X25519 public key."); }
         }
     }
 
-    public static unsafe void GetX25519PrivateKey(Span<byte> X25519PrivateKey, ReadOnlySpan<byte> Ed25519PrivateKey)
+    public static unsafe void GetX25519PrivateKey(Span<byte> x25519PrivateKey, ReadOnlySpan<byte> ed25519PrivateKey)
     {
-        Validation.EqualToSize(nameof(X25519PrivateKey), X25519PrivateKey.Length, X25519.PrivateKeySize);
-        Validation.EqualToSize(nameof(Ed25519PrivateKey), Ed25519PrivateKey.Length, PrivateKeySize);
+        Validation.EqualToSize(nameof(x25519PrivateKey), x25519PrivateKey.Length, X25519.PrivateKeySize);
+        Validation.EqualToSize(nameof(ed25519PrivateKey), ed25519PrivateKey.Length, PrivateKeySize);
         Sodium.Initialize();
-        fixed (byte* x = X25519PrivateKey, e = Ed25519PrivateKey)
+        fixed (byte* x = x25519PrivateKey, e = ed25519PrivateKey)
         {
             int ret = crypto_sign_ed25519_sk_to_curve25519(x, e);
             if (ret != 0) { throw new CryptographicException("Unable to compute X25519 private key."); }
