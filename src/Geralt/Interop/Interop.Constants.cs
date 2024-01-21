4177dd4549c75928717cb131a32b7ce0f021899a
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2024-01-21 12:00:27 +0000

    Interop.Constants.cs: Support iOS.
    iOS also needs to be specified in the <TargetFrameworks>, but that gives an error on my machine. Then the libsodium NuGet needs to be updated to include Android/iOS binaries. Otherwise, the user has to sort this themselves.

--- src/Geralt/Interop/Interop.Constants.cs
+++ src/Geralt/Interop/Interop.Constants.cs
@@ -4,7 +4,11 @@ internal static partial class Interop
 {
     internal static partial class Libsodium
     {
+#if IOS
+        private const string DllName = "__Internal";
+#else
         private const string DllName = "libsodium";
+#endif
         private const CallingConvention Convention = CallingConvention.Cdecl;
     }
 }
