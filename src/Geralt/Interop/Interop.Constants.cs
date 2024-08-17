c81fa0a1915f2b12da0296672d94e65b42630cb7
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2024-08-17 12:33:22 +0100

    Interop.Constants.cs: Get ready for iOS, tvOS, Mac Catalyst, and Android support.
    The Android part comes from here (assuming I'm interpreting it correctly): https://github.com/ektrah/nsec/issues/81

--- src/Geralt/Interop/Interop.Constants.cs
+++ src/Geralt/Interop/Interop.Constants.cs
@@ -4,8 +4,10 @@ internal static partial class Interop
 {
     internal static partial class Libsodium
     {
-#if IOS
+#if IOS || TVOS || MACCATALYST
         private const string DllName = "__Internal";
+#elif ANDROID
+        private const string DllName = "libsodium.so";
 #else
         private const string DllName = "libsodium";
 #endif
