e076592924b63df37be0ae468f9596025ec697e2
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2025-07-20 12:02:42 +0100

    Spans.cs: Tidy up the validation.
    Need to document the possibility of an OverflowException on the website.

--- src/Geralt/Helpers/Spans.cs
+++ src/Geralt/Helpers/Spans.cs
@@ -4,81 +4,46 @@ public static class Spans
 {
     public static void Concat(Span<byte> buffer, ReadOnlySpan<byte> a, ReadOnlySpan<byte> b)
     {
-        checked
-        {
-            Validation.EqualToSize(nameof(buffer), buffer.Length, a.Length + b.Length);
-            Validation.EqualToSize(nameof(a), a.Length, buffer.Length - b.Length);
-            Validation.EqualToSize(nameof(b), b.Length, buffer.Length - a.Length);
-            a.CopyTo(buffer[..a.Length]);
-            b.CopyTo(buffer.Slice(start: a.Length, b.Length));
-        }
+        Validation.EqualToSize(nameof(buffer), buffer.Length, checked(a.Length + b.Length));
+        a.CopyTo(buffer[..a.Length]);
+        b.CopyTo(buffer.Slice(start: a.Length, b.Length));
     }
 
     public static void Concat(Span<byte> buffer, ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, ReadOnlySpan<byte> c)
     {
-        checked
-        {
-            Validation.EqualToSize(nameof(buffer), buffer.Length, a.Length + b.Length + c.Length);
-            Validation.EqualToSize(nameof(a), a.Length, buffer.Length - b.Length - c.Length);
-            Validation.EqualToSize(nameof(b), b.Length, buffer.Length - a.Length - c.Length);
-            Validation.EqualToSize(nameof(c), c.Length, buffer.Length - a.Length - b.Length);
-            a.CopyTo(buffer[..a.Length]);
-            b.CopyTo(buffer.Slice(start: a.Length, b.Length));
-            c.CopyTo(buffer.Slice(start: a.Length + b.Length, c.Length));
-        }
+        Validation.EqualToSize(nameof(buffer), buffer.Length, checked(a.Length + b.Length + c.Length));
+        a.CopyTo(buffer[..a.Length]);
+        b.CopyTo(buffer.Slice(start: a.Length, b.Length));
+        c.CopyTo(buffer.Slice(start: a.Length + b.Length, c.Length));
     }
 
     public static void Concat(Span<byte> buffer, ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, ReadOnlySpan<byte> c, ReadOnlySpan<byte> d)
     {
-        checked
-        {
-            Validation.EqualToSize(nameof(buffer), buffer.Length, a.Length + b.Length + c.Length + d.Length);
-            Validation.EqualToSize(nameof(a), a.Length, buffer.Length - b.Length - c.Length - d.Length);
-            Validation.EqualToSize(nameof(b), b.Length, buffer.Length - a.Length - c.Length - d.Length);
-            Validation.EqualToSize(nameof(c), c.Length, buffer.Length - a.Length - b.Length - d.Length);
-            Validation.EqualToSize(nameof(d), d.Length, buffer.Length - a.Length - b.Length - c.Length);
-            a.CopyTo(buffer[..a.Length]);
-            b.CopyTo(buffer.Slice(start: a.Length, b.Length));
-            c.CopyTo(buffer.Slice(start: a.Length + b.Length, c.Length));
-            d.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length, d.Length));
-        }
+        Validation.EqualToSize(nameof(buffer), buffer.Length, checked(a.Length + b.Length + c.Length + d.Length));
+        a.CopyTo(buffer[..a.Length]);
+        b.CopyTo(buffer.Slice(start: a.Length, b.Length));
+        c.CopyTo(buffer.Slice(start: a.Length + b.Length, c.Length));
+        d.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length, d.Length));
     }
 
     public static void Concat(Span<byte> buffer, ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, ReadOnlySpan<byte> c, ReadOnlySpan<byte> d, ReadOnlySpan<byte> e)
     {
-        checked
-        {
-            Validation.EqualToSize(nameof(buffer), buffer.Length, a.Length + b.Length + c.Length + d.Length + e.Length);
-            Validation.EqualToSize(nameof(a), a.Length, buffer.Length - b.Length - c.Length - d.Length - e.Length);
-            Validation.EqualToSize(nameof(b), b.Length, buffer.Length - a.Length - c.Length - d.Length - e.Length);
-            Validation.EqualToSize(nameof(c), c.Length, buffer.Length - a.Length - b.Length - d.Length - e.Length);
-            Validation.EqualToSize(nameof(d), d.Length, buffer.Length - a.Length - b.Length - c.Length - e.Length);
-            Validation.EqualToSize(nameof(e), e.Length, buffer.Length - a.Length - b.Length - c.Length - d.Length);
-            a.CopyTo(buffer[..a.Length]);
-            b.CopyTo(buffer.Slice(start: a.Length, b.Length));
-            c.CopyTo(buffer.Slice(start: a.Length + b.Length, c.Length));
-            d.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length, d.Length));
-            e.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length + d.Length, e.Length));
-        }
+        Validation.EqualToSize(nameof(buffer), buffer.Length, checked(a.Length + b.Length + c.Length + d.Length + e.Length));
+        a.CopyTo(buffer[..a.Length]);
+        b.CopyTo(buffer.Slice(start: a.Length, b.Length));
+        c.CopyTo(buffer.Slice(start: a.Length + b.Length, c.Length));
+        d.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length, d.Length));
+        e.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length + d.Length, e.Length));
     }
 
     public static void Concat(Span<byte> buffer, ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, ReadOnlySpan<byte> c, ReadOnlySpan<byte> d, ReadOnlySpan<byte> e, ReadOnlySpan<byte> f)
     {
-        checked
-        {
-            Validation.EqualToSize(nameof(buffer), buffer.Length, a.Length + b.Length + c.Length + d.Length + e.Length + f.Length);
-            Validation.EqualToSize(nameof(a), a.Length, buffer.Length - b.Length - c.Length - d.Length - e.Length - f.Length);
-            Validation.EqualToSize(nameof(b), b.Length, buffer.Length - a.Length - c.Length - d.Length - e.Length - f.Length);
-            Validation.EqualToSize(nameof(c), c.Length, buffer.Length - a.Length - b.Length - d.Length - e.Length - f.Length);
-            Validation.EqualToSize(nameof(d), d.Length, buffer.Length - a.Length - b.Length - c.Length - e.Length - f.Length);
-            Validation.EqualToSize(nameof(e), e.Length, buffer.Length - a.Length - b.Length - c.Length - d.Length - f.Length);
-            Validation.EqualToSize(nameof(f), f.Length, buffer.Length - a.Length - b.Length - c.Length - d.Length - e.Length);
-            a.CopyTo(buffer[..a.Length]);
-            b.CopyTo(buffer.Slice(start: a.Length, b.Length));
-            c.CopyTo(buffer.Slice(start: a.Length + b.Length, c.Length));
-            d.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length, d.Length));
-            e.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length + d.Length, e.Length));
-            f.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length + d.Length + e.Length, f.Length));
-        }
+        Validation.EqualToSize(nameof(buffer), buffer.Length, checked(a.Length + b.Length + c.Length + d.Length + e.Length + f.Length));
+        a.CopyTo(buffer[..a.Length]);
+        b.CopyTo(buffer.Slice(start: a.Length, b.Length));
+        c.CopyTo(buffer.Slice(start: a.Length + b.Length, c.Length));
+        d.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length, d.Length));
+        e.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length + d.Length, e.Length));
+        f.CopyTo(buffer.Slice(start: a.Length + b.Length + c.Length + d.Length + e.Length, f.Length));
     }
 }
