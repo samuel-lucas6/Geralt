09b673040c07a0b4f0d2044e6222c23f246bc562
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2025-10-04 12:36:59 +0100

    Interop.Ed25519.cs: Split up the incremental interop.

--- src/Geralt/Interop/Interop.Ed25519.cs
+++ src/Geralt/Interop/Interop.Ed25519.cs
@@ -41,24 +41,5 @@ internal static partial class Interop
         [LibraryImport(DllName)]
         [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
         internal static partial int crypto_sign_verify_detached(ReadOnlySpan<byte> signature, ReadOnlySpan<byte> message, ulong messageLength, ReadOnlySpan<byte> publicKey);
-
-        [LibraryImport(DllName)]
-        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-        internal static partial int crypto_sign_init(ref crypto_sign_state state);
-
-        [LibraryImport(DllName)]
-        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-        internal static partial int crypto_sign_update(ref crypto_sign_state state, ReadOnlySpan<byte> message, ulong messageLength);
-
-        [LibraryImport(DllName)]
-        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-        internal static partial int crypto_sign_final_create(ref crypto_sign_state state, Span<byte> signature, out ulong signatureLength, ReadOnlySpan<byte> privateKey);
-
-        [LibraryImport(DllName)]
-        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-        internal static partial int crypto_sign_final_verify(ref crypto_sign_state state, ReadOnlySpan<byte> signature, ReadOnlySpan<byte> publicKey);
-
-        [StructLayout(LayoutKind.Explicit, Size = 208)]
-        internal struct crypto_sign_state;
     }
 }
---
+++ src/Geralt/Interop/Interop.IncrementalEd25519ph.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    internal static partial class Libsodium
+    {
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_sign_init(ref crypto_sign_state state);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_sign_update(ref crypto_sign_state state, ReadOnlySpan<byte> message, ulong messageLength);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_sign_final_create(ref crypto_sign_state state, Span<byte> signature, out ulong signatureLength, ReadOnlySpan<byte> privateKey);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_sign_final_verify(ref crypto_sign_state state, ReadOnlySpan<byte> signature, ReadOnlySpan<byte> publicKey);
+
+        [StructLayout(LayoutKind.Explicit, Size = 208)]
+        internal struct crypto_sign_state;
+    }
+}
