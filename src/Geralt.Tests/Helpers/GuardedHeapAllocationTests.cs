52bb0d6b086580c168acb4d4913046de48d0bda4
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2025-12-14 12:19:51 +0000

    SecureMemoryTests.cs: Move GuardedHeapAllocation tests to another class.
    Plus a few minor test improvements.

---
+++ src/Geralt.Tests/Helpers/GuardedHeapAllocationTests.cs
@@ -0,0 +1,84 @@
+﻿namespace Geralt.Tests;
+
+[TestClass]
+public class GuardedHeapAllocationTests
+{
+    // With DataRow(), the associated test fails on macOS
+    public static IEnumerable<object[]> InvalidGuardedHeapAllocationSizes()
+    {
+        yield return [0];
+        yield return [Environment.SystemPageSize];
+        yield return [GuardedHeapAllocation.MaxSize + 1];
+    }
+
+    [TestMethod]
+    public void Constants_Valid()
+    {
+        Assert.AreEqual(Environment.SystemPageSize - 16, GuardedHeapAllocation.MaxSize);
+    }
+
+    [TestMethod]
+    public void GuardedHeapAllocation_Valid()
+    {
+        Span<byte> garbage = stackalloc byte[ChaCha20.KeySize];
+        garbage.Fill(0xdb);
+        Span<byte> copy = stackalloc byte[garbage.Length];
+
+        using var secret = new GuardedHeapAllocation(garbage.Length);
+        Span<byte> key = secret.AsSpan();
+        Assert.IsTrue(key.SequenceEqual(garbage));
+
+        RandomNumberGenerator.Fill(key);
+        Assert.IsFalse(key.SequenceEqual(garbage));
+
+        key.CopyTo(copy);
+        secret.ReadOnly();
+        Assert.IsTrue(key.SequenceEqual(copy));
+
+        secret.NoAccess();
+        // Can't check the value
+
+        secret.ReadWrite();
+        RandomNumberGenerator.Fill(key);
+        Assert.IsFalse(key.SequenceEqual(copy));
+    }
+
+    // This test has to be run manually, commenting out parts because there's no way to catch the access violation
+    /*[TestMethod]
+    public void GuardedHeapAllocation_Tampered()
+    {
+        var secret = new GuardedHeapAllocation(ChaCha20.KeySize);
+        Span<byte> key = secret.AsSpan();
+
+        secret.ReadOnly();
+        RandomNumberGenerator.Fill(key);
+
+        //secret.NoAccess();
+        //RandomNumberGenerator.Fill(key);
+
+        //secret.Dispose();
+        //RandomNumberGenerator.Fill(key);
+    }*/
+
+    [TestMethod]
+    [DynamicData(nameof(InvalidGuardedHeapAllocationSizes), DynamicDataSourceType.Method)]
+    public void GuardedHeapAllocation_Invalid(int size)
+    {
+        // This is the only exception that can be tested
+        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => new GuardedHeapAllocation(size));
+    }
+
+    [TestMethod]
+    public void GuardedHeapAllocation_Disposed()
+    {
+        var secret = new GuardedHeapAllocation(ChaCha20.KeySize);
+
+        secret.Dispose();
+
+        Assert.ThrowsExactly<ObjectDisposedException>(() => secret.AsSpan());
+        Assert.ThrowsExactly<ObjectDisposedException>(() => secret.NoAccess());
+        Assert.ThrowsExactly<ObjectDisposedException>(() => secret.ReadOnly());
+        Assert.ThrowsExactly<ObjectDisposedException>(() => secret.ReadWrite());
+        secret.Dispose();
+    }
+}
--- src/Geralt.Tests/Helpers/SecureMemoryTests.cs
+++ src/Geralt.Tests/Helpers/SecureMemoryTests.cs
@@ -3,20 +3,6 @@
 [TestClass]
 public class SecureMemoryTests
 {
-    // With DataRow(), the associated test fails on macOS
-    public static IEnumerable<object[]> InvalidGuardedHeapAllocationSizes()
-    {
-        yield return [0];
-        yield return [Environment.SystemPageSize];
-        yield return [GuardedHeapAllocation.MaxSize + 1];
-    }
-
-    [TestMethod]
-    public void Constants_Valid()
-    {
-        Assert.AreEqual(Environment.SystemPageSize - 16, GuardedHeapAllocation.MaxSize);
-    }
-
     [TestMethod]
     [DataRow(0)]
     [DataRow(ChaCha20.KeySize)]
@@ -53,69 +39,5 @@ public class SecureMemoryTests
         Assert.IsTrue(s.SequenceEqual(new char[s.Length]));
         // Not the same as null or string.Empty
         Assert.IsTrue(s.ToString().All(c => c == '\0'));
-    }
-
-    [TestMethod]
-    public void GuardedHeapAllocation_Valid()
-    {
-        int size = ChaCha20.KeySize;
-        Span<byte> garbage = Enumerable.Repeat((byte)0xdb, size).ToArray();
-        Span<byte> copy = stackalloc byte[size];
-
-        using var secret = new GuardedHeapAllocation(size);
-        Span<byte> key = secret.AsSpan();
-        Assert.IsTrue(key.SequenceEqual(garbage));
-
-        RandomNumberGenerator.Fill(key);
-        Assert.IsFalse(key.SequenceEqual(garbage));
-
-        key.CopyTo(copy);
-        secret.ReadOnly();
-        Assert.IsTrue(key.SequenceEqual(copy));
-
-        secret.NoAccess();
-        // Can't check the value
-
-        secret.ReadWrite();
-        RandomNumberGenerator.Fill(key);
-        Assert.IsFalse(key.SequenceEqual(copy));
-    }
-
-    // This test has to be run manually, commenting out parts because there's no way to catch the access violation
-    /*[TestMethod]
-    public void GuardedHeapAllocation_Tampered()
-    {
-        var secret = new GuardedHeapAllocation(ChaCha20.KeySize);
-        Span<byte> key = secret.AsSpan();
-
-        secret.ReadOnly();
-        RandomNumberGenerator.Fill(key);
-
-        secret.NoAccess();
-        RandomNumberGenerator.Fill(key);
-
-        secret.Dispose();
-        RandomNumberGenerator.Fill(key);
-    }*/
-
-    [TestMethod]
-    [DynamicData(nameof(InvalidGuardedHeapAllocationSizes), DynamicDataSourceType.Method)]
-    public void GuardedHeapAllocation_Invalid(int size)
-    {
-        // This is the only exception that can be tested
-        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => new GuardedHeapAllocation(size));
-    }
-
-    [TestMethod]
-    public void GuardedHeapAllocation_Disposed()
-    {
-        var secret = new GuardedHeapAllocation(ChaCha20.KeySize);
-
-        secret.Dispose();
-
-        Assert.ThrowsExactly<ObjectDisposedException>(() => secret.AsSpan());
-        Assert.ThrowsExactly<ObjectDisposedException>(() => secret.NoAccess());
-        Assert.ThrowsExactly<ObjectDisposedException>(() => secret.ReadOnly());
-        Assert.ThrowsExactly<ObjectDisposedException>(() => secret.ReadWrite());
     }
 }
