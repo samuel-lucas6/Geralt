a871c65670553893e3051e45a569d8b865c874c8
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2025-12-14 12:21:43 +0000

    Interop.SecureMemory.cs: Move GuardedHeapAllocation interop to another file.

---
+++ src/Geralt/Interop/Interop.GuardedHeapAllocation.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    internal static partial class Libsodium
+    {
+        internal const int CANARY_SIZE = 16;
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial IntPtr sodium_malloc(nuint size);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial void sodium_free(IntPtr pointer);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int sodium_mprotect_noaccess(IntPtr pointer);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int sodium_mprotect_readonly(IntPtr pointer);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int sodium_mprotect_readwrite(IntPtr pointer);
+    }
+}
--- src/Geralt/Interop/Interop.SecureMemory.cs
+++ src/Geralt/Interop/Interop.SecureMemory.cs
@@ -5,30 +5,8 @@ internal static partial class Interop
 {
     internal static partial class Libsodium
     {
-        internal const int CANARY_SIZE = 16;
-
         [LibraryImport(DllName)]
         [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
         internal static partial void sodium_memzero(IntPtr pointer, nuint length);
-
-        [LibraryImport(DllName)]
-        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-        internal static partial IntPtr sodium_malloc(nuint size);
-
-        [LibraryImport(DllName)]
-        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-        internal static partial void sodium_free(IntPtr pointer);
-
-        [LibraryImport(DllName)]
-        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-        internal static partial int sodium_mprotect_noaccess(IntPtr pointer);
-
-        [LibraryImport(DllName)]
-        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-        internal static partial int sodium_mprotect_readonly(IntPtr pointer);
-
-        [LibraryImport(DllName)]
-        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-        internal static partial int sodium_mprotect_readwrite(IntPtr pointer);
     }
 }
