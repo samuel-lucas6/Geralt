c25841b496401be1b0022ad1005766979bee8ce8
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2026-01-17 18:17:27 +0000

    Padding.cs: Rename class and code improvements.
    This is a specific type of padding, so it makes sense to name it. Also replaces some validation with a checked statement, rephases 'length' to 'size', passes in a buffer when retrieving the padded length for consistency, and expands the tests. An unpad function hasn't been added to encourage slicing.

--- src/Geralt.Tests/Helpers/Iso78164PaddingTests.cs
+++ src/Geralt.Tests/Helpers/Iso78164PaddingTests.cs
@@ -1,7 +1,7 @@
 namespace Geralt.Tests;
 
 [TestClass]
-public class PaddingTests
+public class Iso78164PaddingTests
 {
     // https://github.com/ektrah/nsec/blob/master/tests/Other/Iso78164PaddingTests.cs
     public static IEnumerable<object[]> NsecTestVectors()
@@ -29,39 +29,57 @@ public class PaddingTests
     public void Pad_Valid(string buffer, string data, int blockSize)
     {
         Span<byte> d = Convert.FromHexString(data);
-        Span<byte> b = stackalloc byte[Padding.GetPaddedLength(d.Length, blockSize)];
+        Span<byte> b = stackalloc byte[Iso78164Padding.GetPaddedBufferSize(d, blockSize)];
 
-        Padding.Pad(b, d, blockSize);
+        Iso78164Padding.Pad(b, d, blockSize);
 
         Assert.AreEqual(buffer, Convert.ToHexString(b).ToLower());
     }
 
     [TestMethod]
     [DataRow(0, 1, 8)]
+    [DataRow(8 + 1, 1, 8)]
+    [DataRow(8 - 1, 1, 8)]
     [DataRow(1, 1, 0)]
     public void Pad_Invalid(int bufferSize, int dataSize, int blockSize)
     {
         var b = new byte[bufferSize];
         var d = new byte[dataSize];
 
-        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => Padding.Pad(b, d, blockSize));
+        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => Iso78164Padding.Pad(b, d, blockSize));
     }
 
     [TestMethod]
-    [DataRow(-1, 8)]
-    [DataRow(1, 0)]
-    public void GetPaddedLength_Invalid(int unpaddedLength, int blockSize)
+    [DataRow(8, 0, 8)]
+    [DataRow(8, 7, 8)]
+    [DataRow(16, 8, 8)]
+    [DataRow(16, 9, 8)]
+    public void GetPaddedBufferSize_Valid(int bufferSize, int dataSize, int blockSize)
     {
-        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => Padding.GetPaddedLength(unpaddedLength, blockSize));
+        Span<byte> d = stackalloc byte[dataSize];
+
+        int paddedSize = Iso78164Padding.GetPaddedBufferSize(d, blockSize);
+
+        Assert.AreEqual(bufferSize, paddedSize);
+    }
+
+    [TestMethod]
+    [DataRow(1, 0)]
+    public void GetPaddedBufferSize_Invalid(int dataSize, int blockSize)
+    {
+        var d = new byte[dataSize];
+
+        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => Iso78164Padding.GetPaddedBufferSize(d, blockSize));
     }
 
     [TestMethod]
     [DataRow("8000000000000000")]
+    [DataRow("80000000000000000000000000000000")]
     public void Fill_Valid(string buffer)
     {
         Span<byte> b = stackalloc byte[buffer.Length / 2];
 
-        Padding.Fill(b);
+        Iso78164Padding.Fill(b);
 
         Assert.AreEqual(buffer, Convert.ToHexString(b).ToLower());
     }
@@ -71,36 +89,39 @@ public class PaddingTests
     {
         var b = Array.Empty<byte>();
 
-        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => Padding.Fill(b));
+        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => Iso78164Padding.Fill(b));
     }
 
     [TestMethod]
     [DynamicData(nameof(NsecTestVectors), DynamicDataSourceType.Method)]
-    public void GetUnpaddedLength_Valid(string paddedData, string data, int blockSize)
+    public void GetUnpaddedBufferSize_Valid(string paddedData, string data, int blockSize)
     {
-        Span<byte> p = Convert.FromHexString(paddedData);
+        Span<byte> pd = Convert.FromHexString(paddedData);
 
-        int unpaddedLength = Padding.GetUnpaddedLength(p, blockSize);
+        int unpaddedSize = Iso78164Padding.GetUnpaddedBufferSize(pd, blockSize);
 
-        Assert.AreEqual(data.Length / 2, unpaddedLength);
+        Assert.AreEqual(data.Length / 2, unpaddedSize);
     }
 
     [TestMethod]
-    [DynamicData(nameof(NsecTestVectors), DynamicDataSourceType.Method)]
-    public void GetUnpaddedLength_Tampered(string paddedData, string data, int blockSize)
+    [DataRow("0102030000000000", 8)]
+    [DataRow("0102038010000000", 8)]
+    [DataRow("0102038000010000", 8)]
+    [DataRow("0102038000000001", 8)]
+    public void GetUnpaddedBufferSize_Tampered(string paddedData, int blockSize)
     {
-        var p = Convert.FromHexString(paddedData.Replace("80", "00"));
+        var pd = Convert.FromHexString(paddedData);
 
-        Assert.ThrowsExactly<FormatException>(() => Padding.GetUnpaddedLength(p, blockSize));
+        Assert.ThrowsExactly<FormatException>(() => Iso78164Padding.GetUnpaddedBufferSize(pd, blockSize));
     }
 
     [TestMethod]
     [DataRow(0, 8)]
     [DataRow(1, 0)]
-    public void GetUnpaddedLength_Invalid(int paddedDataSize, int blockSize)
+    public void GetUnpaddedBufferSize_Invalid(int paddedDataSize, int blockSize)
     {
-        var p = new byte[paddedDataSize];
+        var pd = new byte[paddedDataSize];
 
-        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => Padding.GetUnpaddedLength(p, blockSize));
+        Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => Iso78164Padding.GetUnpaddedBufferSize(pd, blockSize));
     }
 }
---
+++ src/Geralt/Helpers/Iso78164Padding.cs
@@ -0,0 +1,39 @@
+﻿using static Interop.Libsodium;
+
+namespace Geralt;
+
+public static class Iso78164Padding
+{
+    public static void Pad(Span<byte> buffer, ReadOnlySpan<byte> data, int blockSize)
+    {
+        Validation.EqualToSize(nameof(buffer), buffer.Length, GetPaddedBufferSize(data, blockSize));
+        Sodium.Initialize();
+        data.CopyTo(buffer);
+        int ret = sodium_pad(paddedBufferLength: out _, buffer, (nuint)data.Length, (nuint)blockSize, (nuint)buffer.Length);
+        if (ret != 0) { throw new ArgumentOutOfRangeException(nameof(buffer), $"{nameof(buffer)} is not large enough."); }
+    }
+
+    public static int GetPaddedBufferSize(ReadOnlySpan<byte> data, int blockSize)
+    {
+        // data can be empty for Fill()
+        Validation.GreaterThanZero(nameof(blockSize), blockSize);
+        int paddingSize = blockSize - (data.Length % blockSize);
+        return checked(data.Length + paddingSize);
+    }
+
+    public static void Fill(Span<byte> buffer)
+    {
+        Validation.NotEmpty(nameof(buffer), buffer.Length);
+        Pad(buffer, ReadOnlySpan<byte>.Empty, buffer.Length);
+    }
+
+    public static int GetUnpaddedBufferSize(ReadOnlySpan<byte> paddedData, int blockSize)
+    {
+        Validation.NotEmpty(nameof(paddedData), paddedData.Length);
+        Validation.GreaterThanZero(nameof(blockSize), blockSize);
+        Sodium.Initialize();
+        int ret = sodium_unpad(out nuint unpaddedSize, paddedData, (nuint)paddedData.Length, (nuint)blockSize);
+        if (ret != 0) { throw new FormatException("Invalid padding."); }
+        return (int)unpaddedSize;
+    }
+}
