52a5fdb05bab98141572be7be53b957386cffe35
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2026-06-21 15:28:15 +0100

    src: Add X-Wing hybrid KEM.
    The deterministic Encapsulate() function hasn't been implemented because this is documented as being for testing purposes. ML-KEM-768 will be implemented separately, but the other parameter sets haven't been implemented in libsodium and thus will not be present in Geralt.

---
+++ src/Geralt/Crypto/XWing.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+using static Interop.Libsodium;
+
+namespace Geralt;
+
+public static class XWing
+{
+    public const int PublicKeySize = crypto_kem_xwing_PUBLICKEYBYTES;
+    public const int PrivateKeySize = crypto_kem_xwing_SECRETKEYBYTES;
+    public const int SeedSize = crypto_kem_xwing_SEEDBYTES;
+    public const int SharedSecretSize = crypto_kem_xwing_SHAREDSECRETBYTES;
+    public const int CiphertextSize = crypto_kem_xwing_CIPHERTEXTBYTES;
+
+    public static void GenerateKeyPair(Span<byte> publicKey, Span<byte> privateKey)
+    {
+        Validation.EqualTo($"{nameof(publicKey)}.{nameof(publicKey.Length)}", publicKey.Length, PublicKeySize);
+        Validation.EqualTo($"{nameof(privateKey)}.{nameof(privateKey.Length)}", privateKey.Length, PrivateKeySize);
+        Sodium.Initialize();
+        int ret = crypto_kem_xwing_keypair(publicKey, privateKey);
+        if (ret != 0) { throw new CryptographicException("Error generating key pair."); }
+    }
+
+    public static void GenerateKeyPair(Span<byte> publicKey, Span<byte> privateKey, ReadOnlySpan<byte> seed)
+    {
+        Validation.EqualTo($"{nameof(publicKey)}.{nameof(publicKey.Length)}", publicKey.Length, PublicKeySize);
+        Validation.EqualTo($"{nameof(privateKey)}.{nameof(privateKey.Length)}", privateKey.Length, PrivateKeySize);
+        Validation.EqualTo($"{nameof(seed)}.{nameof(seed.Length)}", seed.Length, SeedSize);
+        Sodium.Initialize();
+        int ret = crypto_kem_xwing_seed_keypair(publicKey, privateKey, seed);
+        if (ret != 0) { throw new CryptographicException("Error generating key pair from seed."); }
+    }
+
+    public static void Encapsulate(Span<byte> sharedSecret, Span<byte> ciphertext, ReadOnlySpan<byte> recipientPublicKey)
+    {
+        Validation.EqualTo($"{nameof(sharedSecret)}.{nameof(sharedSecret.Length)}", sharedSecret.Length, SharedSecretSize);
+        Validation.EqualTo($"{nameof(ciphertext)}.{nameof(ciphertext.Length)}", ciphertext.Length, CiphertextSize);
+        Validation.EqualTo($"{nameof(recipientPublicKey)}.{nameof(recipientPublicKey.Length)}", recipientPublicKey.Length, PublicKeySize);
+        Sodium.Initialize();
+        int ret = crypto_kem_xwing_enc(ciphertext, sharedSecret, recipientPublicKey);
+        if (ret != 0) { throw new CryptographicException("Error encapsulating."); }
+    }
+
+    public static void Decapsulate(Span<byte> sharedSecret, ReadOnlySpan<byte> ciphertext, ReadOnlySpan<byte> recipientPrivateKey)
+    {
+        Validation.EqualTo($"{nameof(sharedSecret)}.{nameof(sharedSecret.Length)}", sharedSecret.Length, SharedSecretSize);
+        Validation.EqualTo($"{nameof(ciphertext)}.{nameof(ciphertext.Length)}", ciphertext.Length, CiphertextSize);
+        Validation.EqualTo($"{nameof(recipientPrivateKey)}.{nameof(recipientPrivateKey.Length)}", recipientPrivateKey.Length, PrivateKeySize);
+        Sodium.Initialize();
+        int ret = crypto_kem_xwing_dec(sharedSecret, ciphertext, recipientPrivateKey);
+        if (ret != 0) { throw new CryptographicException("Error decapsulating."); }
+    }
+}
---
+++ src/Geralt/Interop/Interop.XWing.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    // Unused constants/functions have been omitted
+    internal static partial class Libsodium
+    {
+        internal const int crypto_kem_xwing_PUBLICKEYBYTES = 1216;
+        internal const int crypto_kem_xwing_SECRETKEYBYTES = 32;
+        internal const int crypto_kem_xwing_CIPHERTEXTBYTES = 1120;
+        internal const int crypto_kem_xwing_SHAREDSECRETBYTES = 32;
+        internal const int crypto_kem_xwing_SEEDBYTES = 32;
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial nuint crypto_kem_xwing_publickeybytes();
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial nuint crypto_kem_xwing_secretkeybytes();
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial nuint crypto_kem_xwing_ciphertextbytes();
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial nuint crypto_kem_xwing_sharedsecretbytes();
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial nuint crypto_kem_xwing_seedbytes();
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_kem_xwing_keypair(Span<byte> publicKey, Span<byte> privateKey);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_kem_xwing_seed_keypair(Span<byte> publicKey, Span<byte> privateKey, ReadOnlySpan<byte> seed);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_kem_xwing_enc(Span<byte> ciphertext, Span<byte> sharedSecret, ReadOnlySpan<byte> recipientPublicKey);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_kem_xwing_dec(Span<byte> sharedSecret, ReadOnlySpan<byte> ciphertext, ReadOnlySpan<byte> recipientPrivateKey);
+    }
+}
