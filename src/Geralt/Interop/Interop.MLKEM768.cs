ab1cf030dcf46b257968028c568839c9a2490a1c
Samuel Lucas <63159663+samuel-lucas6@users.noreply.github.com>
2026-06-27 14:34:05 +0100

    src: Add ML-KEM-768.
    As an advanced algorithm for non-XWing use cases. Deterministic encapsulation hasn't been implemented because it's only for testing.

---
+++ src/Geralt/Interop/Interop.MLKEM768.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    // Unused functions have been omitted
+    internal static partial class Libsodium
+    {
+        internal const int crypto_kem_mlkem768_PUBLICKEYBYTES = 1184;
+        internal const int crypto_kem_mlkem768_SECRETKEYBYTES = 2400;
+        internal const int crypto_kem_mlkem768_CIPHERTEXTBYTES = 1088;
+        internal const int crypto_kem_mlkem768_SHAREDSECRETBYTES = 32;
+        internal const int crypto_kem_mlkem768_SEEDBYTES = 64;
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial nuint crypto_kem_mlkem768_publickeybytes();
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial nuint crypto_kem_mlkem768_secretkeybytes();
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial nuint crypto_kem_mlkem768_ciphertextbytes();
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial nuint crypto_kem_mlkem768_sharedsecretbytes();
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial nuint crypto_kem_mlkem768_seedbytes();
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_kem_mlkem768_keypair(Span<byte> publicKey, Span<byte> privateKey);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_kem_mlkem768_seed_keypair(Span<byte> publicKey, Span<byte> privateKey, ReadOnlySpan<byte> seed);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_kem_mlkem768_enc(Span<byte> ciphertext, Span<byte> sharedSecret, ReadOnlySpan<byte> recipientPublicKey);
+
+        [LibraryImport(DllName)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        internal static partial int crypto_kem_mlkem768_dec(Span<byte> sharedSecret, ReadOnlySpan<byte> ciphertext, ReadOnlySpan<byte> recipientPrivateKey);
+    }
+}
